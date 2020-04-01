cfdf0a474bc9c571c4fab7602ea23e413332c9af
aryelciu001 <aryelciu001@gmail.com>
2020-04-01 15:46:27 +0800

    Updates

---
+++ precache-manifest.c1cd2c6397cb10a659618dab52d87ad6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17f7c2e070d24c0a831bb7e1dab7c035",
+    "url": "/pathfinder/index.html"
+  },
+  {
+    "revision": "7f493799ab7607f43985",
+    "url": "/pathfinder/static/css/main.c1551830.chunk.css"
+  },
+  {
+    "revision": "f5a2c5d047f1f263c8c8",
+    "url": "/pathfinder/static/js/2.67cccda0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pathfinder/static/js/2.67cccda0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f493799ab7607f43985",
+    "url": "/pathfinder/static/js/main.21d3b55b.chunk.js"
+  },
+  {
+    "revision": "9ccc5e35736b14838482",
+    "url": "/pathfinder/static/js/runtime-main.8f225125.js"
+  }
+]);
